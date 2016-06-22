64a8242b12471130e725ba607d530ef169759841
seaskymonster <wenlaizhang@gmail.com>
2016-06-22 02:03:53 -0700

    [191. Number of 1 Bits][Accepted]committed by LeetCode Extension

--- 191-Number-of-1-Bits/solution.java
+++ 191-Number-of-1-Bits/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int bits = 0;
-        while(n != 0){
+        while(n != 0){ // cannot use n >=1 why?
             if((n & 1) == 1){ //the argument n is regarded as unsigned by us but signed by computer, so using "n % 2 == 1" to check if the lowest bit is 1 is wrong because it doesn't concern the highest bit indicate negative value. So we use "&" operation instead to check the lowest bit, and we use ">>>" (not ">>") to do logical shift right.
                 bits++;
             }
