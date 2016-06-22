d6215177856e66459823b94ef642592a4d410d84
seaskymonster <wenlaizhang@gmail.com>
2016-06-22 02:11:23 -0700

    [7. Reverse Integer][Compile Error]committed by LeetCode Extension

---
+++ 7-Reverse-Integer/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        while(x != 0){
+            result = result*10 + x%10;
+            x = x/10;
+        }
+        if(x < Integer.MIN_VALUE || x > Integer.MAX_VALUE){
+            return 0;
+        }
+        return result;
+    }
+}
