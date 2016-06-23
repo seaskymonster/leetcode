8f099606926cf0260e4d2718ead8706b37136c8d
seaskymonster <wenlaizhang@gmail.com>
2016-06-23 01:24:02 -0700

    [136. Single Number][Accepted]committed by LeetCode Extension

---
+++ 136-Single-Number/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int result = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
