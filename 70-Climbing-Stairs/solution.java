5b5c5ca012732c92f90da680a70df2ac4cf13376
seaskymonster <wenlaizhang@gmail.com>
2016-06-22 23:56:10 -0700

    [70. Climbing Stairs][Compile Error]committed by LeetCode Extension

--- 70-Climbing-Stairs/solution.java
+++ 70-Climbing-Stairs/solution.java
@@ -2,7 +2,9 @@ public class Solution {
     public int climbStairs(int n) {
         if(n == 1) return 1;
         if(n == 2) return 2;
-        int[] f = new int[n+1]; // f[i] represents different ways to the i step
+        f[1] = 1;
+        f[2] = 2;
+        int[] f = new int[n+2]; // f[i] represents different ways to the i step
         for(int i = 3; i <= n+1; i++){
             f[i] = f[i-1] + f[i-2];
         }
