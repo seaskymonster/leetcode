4394d17fbc5df9168b649e9f0cb61406fda1f61b
seaskymonster <wenlaizhang@gmail.com>
2016-06-24 02:31:50 -0700

    [98. Validate Binary Search Tree][Compile Error]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -8,14 +8,30 @@
  * }
  */
 public class Solution {
-    public boolean isValidBST(TreeNode root) {
+    // public boolean isValidBST(TreeNode root) { // the method below is wrong.....not only test every node, left half shold
+    // less than the root, right half should greater than root.
+    //     if(root == null){
+    //         return true;
+    //     }
+        
+    //     boolean left = isValidBST(root.left);
+    //     boolean right = isValidBST(root.right);
+        
+    //     return left && right && (root.left == null ||(root.left !=null && root.left.val < root.val)) && (root.right == null ||(root.right !=null && root.right.val > root.val)); 
+    // }
+    
+    
+       public boolean isValidBST(TreeNode root) {
         if(root == null){
             return true;
         }
-        
-        boolean left = isValidBST(root.left);
-        boolean right = isValidBST(root.right);
-        
-        return left && right && (root.left == null ||(root.left !=null && root.left.val < root.val)) && (root.right == null ||(root.right !=null && root.right.val > root.val))
-    }
+        return helper(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+       }
+       
+       public boolean (TreeNode root, int max, int min){
+           if(root == null){
+               return true;
+           }
+           return root.val < max && root.val > min && helper(root.left, root.val, min) && helper(root.right, max, root.val);
+       }
 }
