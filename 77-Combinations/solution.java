c038bd7ab8632aaf1cdca2883f54f651335d96e7
seaskymonster <wenlaizhang@gmail.com>
2016-06-24 00:49:53 -0700

    [77. Combinations][Compile Error]committed by LeetCode Extension

---
+++ 77-Combinations/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+      List<List<Integer>> result = new ArrayList<Integer>();
+      if(n < k){
+          return result;
+      }
+      List<Integer> path = new ArrayList<Integer>();
+      int pos = 1;
+      helper(result, path, pos, n, k);
+      return result;
+    }
+    
+    public void helper(List<List<Integer>> result, List<Integer> path, int pos, int n, int k){
+        if(k == 0){
+            result.add(path);
+        }
+        for(int i = pos; i < n; i++){
+            path.add(i);
+            helper(result, path, pos+1, n, k-1);
+            path.remove(path.size()-1);
+        }
+    }
+    
+    
+}
