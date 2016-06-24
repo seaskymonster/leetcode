fe84a637bfbbb966a1566c02e18f6899a5258527
seaskymonster <wenlaizhang@gmail.com>
2016-06-24 03:16:59 -0700

    [121. Best Time to Buy and Sell Stock][Compile Error]committed by LeetCode Extension

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            m = prices[i] < min ? prices[i]:min;
+            profit = prices[i]-min > profit ? prices[i]-min : profit;
+        }
+        return profit;
+    }
+}
