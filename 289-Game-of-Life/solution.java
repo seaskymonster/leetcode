935ee68f9ae58c28b8d5f50c94b50b8a2e1f76d9
seaskymonster <wenlaizhang@gmail.com>
2016-06-24 22:40:21 -0700

    [289. Game of Life][Accepted]committed by LeetCode Extension

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -30,7 +30,7 @@ public class Solution {
     public int neighbors(int[][] board, int i, int j){
         int result = 0;
         for(int k = i-1; k <= i+1 ;k++){
-            for(int l = j-1; j <= j+1; j++){
+            for(int l = j-1; l <= j+1; l++){
                 if(k>=0 && k <= board.length-1 && l >=0 && l<= board[0].length-1){
                     if((board[k][l]&1) == 1){
                        result++;
