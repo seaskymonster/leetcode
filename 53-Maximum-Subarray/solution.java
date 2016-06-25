6086f314042aee13cf0b77ec3cfe4917783fed92
seaskymonster <wenlaizhang@gmail.com>
2016-06-24 21:04:10 -0700

    [53. Maximum Subarray][Compile Error]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,18 +1,37 @@
 public class Solution {
-    public int maxSubArray(int[] nums) {
+    // public int maxSubArray(int[] nums) {
+    //     if(nums.length == 0) {
+    //         return 0;
+    //     }
+    //     int[] f = new int[nums.length];
+    //     f[0] = nums[0];
+    //     for(int i = 1; i < nums.length; i++){
+    //         //f[i] = Math.max(f[i-1] + nums[i], nums[i]); 
+    //         f[i] = f[i-1] >=0 ? f[i-1]+nums[i] : nums[i];// the maxsum before is a negtive, drop it, if it is positive ,use it
+    //     }
+    //     int result = Integer.MIN_VALUE;
+    //     for(int j = 0; j < nums.length; j++){
+    //         result = f[j] > result ? f[j] : result;
+    //     }
+    //     return result;
+    // }
+    
+      public int maxSubArray(int[] nums) {
         if(nums.length == 0) {
             return 0;
         }
-        int[] f = new int[nums.length];
-        f[0] = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            //f[i] = Math.max(f[i-1] + nums[i], nums[i]); 
-            f[i] = f[i-1] >=0 ? f[i-1]+nums[i] : nums[i];// the maxsum before is a negtive, drop it, if it is positive ,use it
+        int[] sum = new int[nums.length+1];
+        sum[0] = 0;
+        for(int i = 1; i < sum.length; i++){
+            sum[i] = sum[i-1] + nums[i-1];
         }
-        int result = Integer.MIN_VALUE;
-        for(int j = 0; j < nums.length; j++){
-            result = f[j] > result ? f[j] : result;
+        
+        int min = Integer.MAX_VALUE;
+        int maxsub = 0;
+        for (int j = 0; j < sum.length; j++){
+            maxsub = Math.max(maxsub, sum[i]-min);
+            min = Math.min(min, sub[i]);
         }
-        return result;
+        return maxsub;
     }
 }
