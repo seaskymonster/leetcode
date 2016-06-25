644c3f943c39f39e8ddcfcd083a75c51fc573920
seaskymonster <wenlaizhang@gmail.com>
2016-06-25 16:16:58 -0700

    [228. Summary Ranges][Compile Error]committed by LeetCode Extension

---
+++ 228-Summary-Ranges/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<String>();
+        if(nums == null || nums.length == 0){
+            return result;
+        }
+        StringBuilder sb = new StringBuilder();
+        boolean newstring = true;
+        for( int i = 0; i < nums.length; i++){
+            if(newstring){
+                sb.append(nums[i]);
+                newstring = false;
+            }else{
+                if(nums[i] != nums[i-1] + 1){
+                   sb.append(" ").append(nums[i]);
+                   result.add(sb);
+                   sb = new StringBuilder();
+                   newstring = true;
+                }else{
+                   countinue;
+                }
+           }
+        }
+        if(newstring == false){
+            result.add(sb);
+        }
+        
+        return result;
+        
+    }
+}
