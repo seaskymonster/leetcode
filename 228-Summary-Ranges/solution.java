345793a85e87d6e31a91122870132d3ec79ad600
seaskymonster <wenlaizhang@gmail.com>
2016-06-25 17:58:41 -0700

    [228. Summary Ranges][Compile Error]committed by LeetCode Extension

--- 228-Summary-Ranges/solution.java
+++ 228-Summary-Ranges/solution.java
@@ -1,23 +1,44 @@
 public class Solution {
-    public List<String> summaryRanges(int[] nums) {
+    // public List<String> summaryRanges(int[] nums) {
+    //     List<String> result = new ArrayList<String>();
+    //     if(nums == null || nums.length == 0){
+    //         return result;
+    //     }
+    //     int start = 0;
+    //     StringBuilder sb = new StringBuilder();
+    //     for( int i = 0; i < nums.length; i++){
+    //       if(i == nums.length-1 || nums[i] != nums[i+1] - 1 ){
+    //             sb.append(nums[start]);
+    //             if(start != i){
+    //               sb.append("->").append(nums[i]);
+    //             }
+    //             result.add(sb.toString());
+    //             sb = new StringBuilder();
+    //             start = i+1;
+    //          }else{
+    //               continue;
+    //          }
+    //     }
+       
+    //     return result;
+        
+    // }
+    
+      public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<String>();
         if(nums == null || nums.length == 0){
             return result;
         }
-        int start = 0;
-        StringBuilder sb = new StringBuilder();
-        for( int i = 0; i < nums.length; i++){
-           if(i == nums.length-1 || nums[i] != nums[i+1] - 1 ){
-                sb.append(nums[start]);
-                if(start != i){
-                   sb.append("->").append(nums[i]);
-                }
-                result.add(sb.toString());
-                sb = new StringBuilder();
-                start = i+1;
-             }else{
-                   continue;
-             }
+      
+        for( int i = 0; j = 0; i < nums.length; i++){
+          if(i= nums[i].length-1 || nums[i] != nums[i+1]-1){
+              if(i == j){
+                  result.add(nums[i] + "");
+              }else{
+                  result.add(nums[j] + "->" + nums[i])
+              }
+              j = i+1;
+          }
         }
        
         return result;
