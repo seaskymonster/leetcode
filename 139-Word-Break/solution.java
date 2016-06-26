fad30c49d3e4f81f54cec0ceac52d102934f935a
seaskymonster <wenlaizhang@gmail.com>
2016-06-26 14:55:51 -0700

    [139. Word Break][Compile Error]committed by LeetCode Extension

---
+++ 139-Word-Break/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+       boolean[] word = new boolean [s.length()+1];// word[i] means substring from 0 to i is a perfect word
+       word[0] = true;
+       for(int i = 1; i <= s.length(); i++){
+           for(int j = 1; j <= i ; j++){
+               if(!word[j]){
+                   continue;
+               }else{
+                   if(wordDict.contains(s.substring(j, i)){
+                       word[i] = true;
+                       break;
+                   }
+               }
+           }
+       }
+       return word[word.length()-1];
+    }
+}
