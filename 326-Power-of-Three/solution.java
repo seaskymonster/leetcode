c837896b04d5aafbf3ea00294f62055633f0f156
seaskymonster <wenlaizhang@gmail.com>
2016-06-25 19:49:17 -0700

    [326. Power of Three][Accepted]committed by LeetCode Extension

--- 326-Power-of-Three/solution.java
+++ 326-Power-of-Three/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public boolean isPowerOfThree(int n) {
-       if(n < 1) return false;
+      if(n < 1) return false;
        
-       while(n % 3 == 0){
-           n = n/3;
-       }
+      while(n % 3 == 0){
+          n = n/3;
+      } // go until n is 1. every step you should make sure n%3, becuase 4/3 =1.  也就是确认没有约等于的情况发生。
        
-       return n == 1;
+      return n == 1;
       
     }
 }
