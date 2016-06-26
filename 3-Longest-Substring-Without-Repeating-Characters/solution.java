7b330193eae8ba12e247a70e8807f9be5715d9ef
seaskymonster <wenlaizhang@gmail.com>
2016-06-25 20:30:33 -0700

    [3. Longest Substring Without Repeating Characters][Accepted]committed by LeetCode Extension

--- 3-Longest-Substring-Without-Repeating-Characters/solution.java
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.java
@@ -1,13 +1,15 @@
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0 ) return 0;
+        if(s.length() == 1) return 1;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         int length = 0;
         int start = 0;
         for( int i = 0 ; i < s.length(); i++){
-            if(map.containsKey(s.charAt(i)){
-                if(map.get(s.charAt(i) >= start){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i)) >= start){
                   length = Math.max(length, i-start);
-                 start = i;
+                 start = map.get(s.charAt(i)) +1;
                 }
                 map.put(s.charAt(i), i);
             }else{
