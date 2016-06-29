541c61261ec634fd8ac7e94a8a4d843e7409461c
seaskymonster <wenlaizhang@gmail.com>
2016-06-28 23:37:12 -0700

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -4,11 +4,11 @@ public class Solution {
         Arrays.fill(isPrime, true);
         
         for(int i = 2; i < n ; i++){
-            
+            if(isPrime[i]){ // 如果当前数是prime，把他的2倍，3倍，4倍，扫一下，如果他不是prime，不用扫了，因为可以证明，他的2倍，3倍，4倍已经被扫过了。
                 for(int j = i + i; j < n; j = j + i){
                     isPrime[j] = false;
                 }
-            
+            }
         }
         
         int count = 0;
