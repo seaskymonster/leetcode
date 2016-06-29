1d272928e49cbca568b77691e0837915c15dc9e1
seaskymonster <wenlaizhang@gmail.com>
2016-06-29 00:22:05 -0700

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -3,47 +3,47 @@ public class Solution {
         int m = board.length;
         int n = board[0].length;
         
-        HashSet<Character> set = new HashSet<Charcter>();
+        HashSet<Character> set = new HashSet<Character>();
         
         for(int i = 0; i < m; i++){
+            set.clear();
             for(int j = 0; j < n; j++){
                 if(board[i][j] != '.'){
-                    if(set.contains(board[i][j]){
+                    if(set.contains(board[i][j])){
                         return false;
                     }else{
                         set.add(board[i][j]);
                     }
                 }
             }
-            set.clear();
         }
         
         set.clear();
          for(int i = 0; i < n; i++){
+            set.clear();
             for(int j = 0; j < m; j++){
                 if(board[j][i] != '.'){
-                    if(set.contains(board[j][i]){
+                    if(set.contains(board[j][i])){
                         return false;
                     }else{
                         set.add(board[j][i]);
                     }
                 }
             }
-            set.clear();
         }
         
-        set.clear();
         for(int i = 0; i < m; i = i + 3){
             for (int j = 0; j < n; j = j + 3){
+                 set.clear();
+
                 for ( int k = 0; k < 9; k++){
                     if(board[i + k/3][j + k%3] != '.'){
-                    if(set.contains(board[i + k/3][j + k%3]){
+                    if(set.contains(board[i + k/3][j + k%3])){
                         return false;
                     }else{
                         set.add(board[i + k/3][j + k%3]);
                     }
                  }
-                 set.clear();
               }
             }
         }
