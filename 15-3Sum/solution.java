e688990024aba43857aa54a765a94fb7abd7ed6a
seaskymonster <wenlaizhang@gmail.com>
2016-07-01 00:20:25 -0700

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        if(nums == null || nums.length <= 3 ){
+        if(nums == null || nums.length < 3 ){
             return result;
         }
         Arrays.sort(nums);
         for(int i = 0; i < nums.length-2; i++){
-            if(i > 1 && nums[i] == nums[i-1]){
+            if(i >= 1 && nums[i] == nums[i-1]){
                 continue;
             }
             int target = 0 - nums[i];
@@ -19,10 +19,12 @@ public class Solution {
                     path.add(nums[start]);
                     path.add(nums[end]);
                     result.add(new ArrayList<>(path));
-                    while(start < end && nums[start+1] == nums[start]){
+                    start++;
+                    end--;
+                    while(start < end && nums[start] == nums[start-1]){
                         start++; // 这里不能出错
                     }
-                    while(start < end && nums[end-1] == nums[end]){
+                    while(start < end && nums[end] == nums[end+1]){
                         end--; //这里不能出错
                     }
                 }else if(nums[start] + nums[end] > target){
