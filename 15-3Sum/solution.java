ff0169b59c339ae02ff7a6f4cbbea820c8b92118
seaskymonster <wenlaizhang@gmail.com>
2016-06-30 23:53:19 -0700

    [15. 3Sum][Compile Error]committed by LeetCode Extension

---
+++ 15-3Sum/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.lenght <=2 ){
+            return result;
+        }
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-2; i++){
+            int target = 0 - nums[i];
+            int start = i+1; //这里不能出错。
+            int end = nums.length -1;
+            while(start < end){
+                if(nums[start] + nums[end] == target){
+                    List<Integer> path = new ArrayList<Integer>();
+                    path.add(target);
+                    path.add(nums[start]);
+                    path.add(nums[end]);
+                    result.add(new ArrayList<>(path));
+                }else if(nums[start] + nums[end] > target){
+                    end--;
+                }else{
+                    start++;
+                }
+            }
+        }
+       return result;
+    }
+}
