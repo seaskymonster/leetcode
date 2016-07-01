66163089bcbccbae0e992b907805ef0233269b58
seaskymonster <wenlaizhang@gmail.com>
2016-07-01 00:03:53 -0700

    [15. 3Sum][Time Limit Exceeded]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -6,16 +6,25 @@ public class Solution {
         }
         Arrays.sort(nums);
         for(int i = 0; i < nums.length-2; i++){
+            if(i > 1 && nums[i] == nums[i-1]){
+                continue;
+            }
             int target = 0 - nums[i];
             int start = i+1; //这里不能出错。
             int end = nums.length -1;
             while(start < end){
                 if(nums[start] + nums[end] == target){
                     List<Integer> path = new ArrayList<Integer>();
-                    path.add(target);
+                    path.add(nums[i]); //这里不能出错；
                     path.add(nums[start]);
                     path.add(nums[end]);
                     result.add(new ArrayList<>(path));
+                    while(nums[start+1] == nums[start] && start < end){
+                        start++; // 这里不能出错
+                    }
+                    while(nums[end-1] == nums[end] && start <end){
+                        end--; //这里不能出错
+                    }
                 }else if(nums[start] + nums[end] > target){
                     end--;
                 }else{
