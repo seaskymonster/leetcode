2d83ae80cb8b3e1036c30c717086f99e94fe9788
seaskymonster <wenlaizhang@gmail.com>
2016-07-02 14:46:51 -0700

    [151. Reverse Words in a String][Runtime Error]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -6,25 +6,25 @@ public class Solution {
         String[] sa = s.split(" ");
         StringBuilder sb = new StringBuilder();
         for(int i = s.length() -1 ; i >= 0 ; i--){
-            sb = sb + reverse(sa[i]) + " ";
+            sb = sb.append(sa[i]).append(" ");
         }
         return sb.substring(0, sb.length()-1);
     }
     
-    public String reverse(String s) {
-        if(s == null || s.length() == 0){
-            return s;
-        }
-        char[] ca = s.toCharArray();
-        int i = 0;
-        int j = ca.length-1;
-        while(i < j){
-            char tmp = ca[i];
-            ca[i] = ca[j];
-            ca[j] = tmp;
-            i++;
-            j--;
-        }
-        return ca.toString();
-    }
+    // public String reverse(String s) {
+    //     if(s == null || s.length() == 0){
+    //         return s;
+    //     }
+    //     char[] ca = s.toCharArray();
+    //     int i = 0;
+    //     int j = ca.length-1;
+    //     while(i < j){
+    //         char tmp = ca[i];
+    //         ca[i] = ca[j];
+    //         ca[j] = tmp;
+    //         i++;
+    //         j--;
+    //     }
+    //     return ca.toString();
+    // }
 }
