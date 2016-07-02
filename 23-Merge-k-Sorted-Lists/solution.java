194f6a796ca47a762a5e5f0b7f94e3a7996be08e
seaskymonster <wenlaizhang@gmail.com>
2016-07-02 02:42:30 -0700

    [23. Merge k Sorted Lists][Compile Error]committed by LeetCode Extension

---
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private Comparator<ListNode> ListNodeComparator = new Comparator<ListNode>(){
+        public int compare(ListNode left, ListNode right){
+            if(left == null){
+                return 1;
+            }else(right == null){
+                return -1;
+            }
+            return left.val - right.val;
+        }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.size() == 0) {
+            return null;
+        }
+        
+        Queue<ListNode> heap = new PriorityQueue<ListNode>(lists.size(), ListNodeComparator);
+        for(int i = 0; i < lists.size(); i++){
+           if (lists.get(i) != null) {
+                heap.add(lists.get(i));
+            }
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(!heap.isEmpty()){
+            ListNode head = heap.poll();
+            tail.next = head;
+            if (head.next != null) {
+                heap.add(head.next);
+            }
+        }
+        
+        return dummy.next;
+
+    }
+}
