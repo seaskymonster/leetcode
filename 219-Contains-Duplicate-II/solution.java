1aca8984babb607b1e0d31acb30a5570161d492f
seaskymonster <wenlaizhang@gmail.com>
2016-07-02 23:46:54 -0700

    [219. Contains Duplicate II][Compile Error]committed by LeetCode Extension

---
+++ 219-Contains-Duplicate-II/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+      if(nums == null || nums.length <= 1){
+          return false;
+      }
+      HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+      for(int i = 0; i < nums.length; i++){
+          if(map.contains(nums[i]){
+              if(i - map.get(nums[i]) <= k){
+                  return true;
+              }
+          }
+          map.put(nums[i], i);
+      }
+      return false;
+    }
+}
