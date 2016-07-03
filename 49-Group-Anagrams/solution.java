96f2b71b6e25a89a5a81df4f3ce0bc083103ae2b
seaskymonster <wenlaizhang@gmail.com>
2016-07-03 16:43:42 -0700

    [49. Group Anagrams][Accepted]committed by LeetCode Extension

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -4,7 +4,8 @@ public class Solution {
         for(String s: strs){
             char[] array = s.toCharArray();
             Arrays.sort(array);
-            String sc = String.valueOf(array);;
+            //String sc = array.toString(); // 生成一个新的object，这个是错的。
+            String sc = String.valueOf(array);
             if(map.containsKey(sc)){
                 List<String> list = map.get(sc);
                 list.add(s);
