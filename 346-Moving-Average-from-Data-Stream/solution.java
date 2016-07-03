f7ee9232ad2f987c66e4c09acf5978d078940591
seaskymonster <wenlaizhang@gmail.com>
2016-07-03 01:58:06 -0700

    [346. Moving Average from Data Stream][Compile Error]committed by LeetCode Extension

--- 346-Moving-Average-from-Data-Stream/solution.java
+++ 346-Moving-Average-from-Data-Stream/solution.java
@@ -9,7 +9,8 @@ public class MovingAverage {
     
     public double next(int val) {
         for(int i = 0; i <= index; i++){
-            int sum += array[index];
+            int sum = 0;
+            sum += array[index];
         }
         return (sum+array[++index])/(index+1);
     }
