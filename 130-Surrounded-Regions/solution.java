3925fc7af2896b754eca4d7ee2f31aff422f384d
seaskymonster <wenlaizhang@gmail.com>
2016-07-04 16:46:51 -0700

    [130. Surrounded Regions][Runtime Error]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -4,19 +4,19 @@ public class Solution {
             for(int j = 0; j <board[0].length; j++){
                 if(i == 0 || i == board.length -1 || j == 0 ||j == board[0].length-1){
                     if(board[i][j] == 'O'){
-                        board[i][j] = '*';
+                        dfs(board, i ,j);
                     }
                 }
             }
         }
         
-         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j <board[0].length; j++){
-                 if(board[i][j] == '*'){
-                        dfs(board, i ,j);
-                  }
-            }
-        }
+        //  for(int i = 0; i < board.length; i++){
+        //     for(int j = 0; j <board[0].length; j++){
+        //          if(board[i][j] == '*'){
+        //                 dfs(board, i ,j);
+        //           }
+        //     }
+        // }
         
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j <board[0].length; j++){
@@ -33,7 +33,7 @@ public class Solution {
          if(i < 0 || j < 0 || i >= board.length || j >= board[0].length){
             return;
         }
-        if(board[i][j] == 'O' || board[i][j] == '*'){
+        if(board[i][j] == 'O'){
             board[i][j] = '*';
             dfs(board, i-1, j);
             dfs(board, i+1, j);
