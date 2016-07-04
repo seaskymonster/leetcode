8af7b78a894afd7e47f12578ed3f5a2bf4c7a38b
seaskymonster <wenlaizhang@gmail.com>
2016-07-03 21:51:10 -0700

    [280. Wiggle Sort][Compile Error]committed by LeetCode Extension

---
+++ 280-Wiggle-Sort/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        for(int i = 1 ; i < nums.length; i++){
+            if(i%2 == 0){
+                if(nums[i] > nums[i-1]){
+                    swap(nums, i-1, i);
+                }
+            }else{
+                if(nums[i] < nums[i-1]){
+                    swap(nums, i-1, i);
+                }
+            }
+        }
+    }
+    
+    public void swap(int[] nums, int left, int end){
+        int tmp = nums[end];
+        nums[end] = nums[start];
+        nums[start] = tmp;
+    }
+}
