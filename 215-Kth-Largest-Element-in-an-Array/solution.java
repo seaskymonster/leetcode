a20420560c36b300ad8f51ed6efbeb14a54df1f3
seaskymonster <wenlaizhang@gmail.com>
2016-07-04 15:18:14 -0700

    [215. Kth Largest Element in an Array][Runtime Error]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
         int n = nums.length;
-        int p = partition(nums, 0, n - 1, n - k + 1);
+        int p = partition(nums, 0, n - 1, n-k+1);
         return nums[p];
     }
     
@@ -11,16 +11,16 @@ public class Solution {
         int pivot = nums[middle];
         int i = left; 
         int j = right;
-        while (i <= j) {
-			while (i <= j && nums[i] < pivot) {
+        while (i < j) {
+			while (i < j && nums[i] < pivot) {
 				i++;
 			}
  
-			while (i <= j && nums[j] > pivot) {
+			while (i < j && nums[j] > pivot) {
 				j--;
 			}
  
-			if (i <= j) {
+			if (i < j) {
 				int temp = nums[i];
 				nums[i] = nums[j];
 				nums[j] = temp;
@@ -32,9 +32,9 @@ public class Solution {
 		int m = i - left + 1;
 		if(m == k) return i;
 		if(m > k){
-		    return partition(nums, left, i-1, k);
+		    return partition(nums, left, i-1, k-m);
 		}else{
-		    return partition(nums, i+1, right, k-m); // k -m 中的m是找左边有m个已经被去除。
+		    return partition(nums, i+1, right, k); // k -m 中的m是找左边有m个已经被去除。
 		}
     }
 }
