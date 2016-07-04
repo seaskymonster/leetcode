cb3ae2cad00d1611ff55a0336b9016f4c36b645a
seaskymonster <wenlaizhang@gmail.com>
2016-07-04 16:47:43 -0700

    [130. Surrounded Regions][Runtime Error]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -33,6 +33,9 @@ public class Solution {
          if(i < 0 || j < 0 || i >= board.length || j >= board[0].length){
             return;
         }
+        if(board[i][j] == '*' || board[i][j] == 'X'){
+            return;
+        }
         if(board[i][j] == 'O'){
             board[i][j] = '*';
             dfs(board, i-1, j);
