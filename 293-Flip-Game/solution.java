2d9c7762f79af76cb858c2c679a8c871505b0f0b
seaskymonster <wenlaizhang@gmail.com>
2016-07-03 21:30:13 -0700

    [293. Flip Game][Accepted]committed by LeetCode Extension

---
+++ 293-Flip-Game/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> result = new ArrayList<String>();
+        if(s == null || s.length() <=1){
+            return result;
+        }
+        
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                char[] array = s.toCharArray();
+                if(array[i] == '+'){
+                   array[i-1] = array[i] = '-';
+                   result.add(String.valueOf(array));
+
+                }
+            }
+        }
+        return result;
+    }
+}
