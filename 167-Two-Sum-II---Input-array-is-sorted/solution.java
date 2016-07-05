cebbeeba01270812f59772f6f05d3374edca9779
seaskymonster <wenlaizhang@gmail.com>
2016-07-04 19:07:45 -0700

    [167. Two Sum II - Input array is sorted][Accepted]committed by LeetCode Extension

--- 167-Two-Sum-II---Input-array-is-sorted/solution.java
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -5,8 +5,8 @@ public class Solution {
         int j = numbers.length - 1;
         while(i < j){
             if(numbers[i] + numbers[j] == target){
-                int[0] = i;
-                int[1] = j;
+                result[0] = i+1;
+                result[1] = j+1;
                 return result;
             }else if (numbers[i] + numbers[j] > target){
                 j--;
