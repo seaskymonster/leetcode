d57903f9c3daff28da5103e153c1267d6c702f1b
seaskymonster <wenlaizhang@gmail.com>
2016-07-05 01:46:40 -0700

    [66. Plus One][Accepted]committed by LeetCode Extension

--- 66-Plus-One/solution.java
+++ 66-Plus-One/solution.java
@@ -1,18 +1,25 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
-        int carry = 1;
-        for(int i = digits.length-1; i >= 0; i--){
-            int sum = digits[i] + carry;
-            digits[i] = sum%10;
-            carry = sum/10;
+        if(digits == null || digits.length == 0){
+            return digits;
         }
-        if(carry == 0){
+        
+        int carry = 0;
+        for(int i = digits.length - 1; i >= 0; i--){
+            int value = digits[i];
+            digits[i] = (value + 1) % 10;
+            carry = (value + 1) / 10;
+            if(carry == 0) {
+                break;
+            }
+        }
+        if(carry == 0) {
             return digits;
         }else{
-            int[] result = new int[digits.length + 1];
+            int[] result = new int[digits.length+1];
             result[0] = 1;
-            for(int j = 1; j < result.length; j++){
-                result[j] = digits[j-1];
+            for(int i = 1; i < result.length; i++){
+                result[i] = digits[i-1];
             }
             return result;
         }
