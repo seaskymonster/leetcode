c6727c741b269727cc985c57f53ec4420e82dd0e
seaskymonster <wenlaizhang@gmail.com>
2016-07-04 19:01:31 -0700

    [88. Merge Sorted Array][Compile Error]committed by LeetCode Extension

---
+++ 88-Merge-Sorted-Array/solution.java
@@ -0,0 +1,45 @@
+public class Solution {
+    // public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //     int[] temp = new int[m+n];
+    //     int left = 0;
+    //     int right = 0;
+    //     int index = 0;
+    //     while(left<m && right<n){
+    //         if(nums1[left]<nums2[right]){
+    //             temp[index++]= nums1[left++];
+    //         }else{
+    //             temp[index++] = nums2[right++];
+    //         }
+    //     }
+    //     while(left<m){
+    //         temp[index++] = nums1[left++];
+    //     }
+    //     while(right<n){
+    //         temp[index++] = nums2[right++];
+    //     }
+    //     for(int i=0;i<m+n;i++){
+    //         nums1[i] = temp[i];
+    //     }
+    // }
+    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       int p1 = m-1;
+       int p2 = n-1;
+       int p = nums1.length-1;
+       
+       while(p1 >= 0 && p2 >= 0){
+           if(nums1[p1] > nums[p2]){
+               nums1[p--] = nums1[p1--];
+           }else{
+               nums1[p--] = nums2[p2--];
+           }
+       }
+       while(p1 >= 0){
+           nums1[p--] = nums1[p1--];
+       }
+       
+       while(p2 >= 0){
+           nums1[p--] = nums2[p2--];
+       }
+    }
+}
