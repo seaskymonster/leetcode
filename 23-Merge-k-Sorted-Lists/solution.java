153670dc499f68bbe275961c3d0eb5e3157b55dd
seaskymonster <wenlaizhang@gmail.com>
2016-07-04 19:28:53 -0700

    [23. Merge k Sorted Lists][Runtime Error]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -7,43 +7,35 @@
  * }
  */
 public class Solution {
-    private Comparator<ListNode> ListNodeComparator = new Comparator<ListNode>(){ // implement 一个comparator，然后重写compare函数
-        public int compare(ListNode left, ListNode right){
-            if(left == null){
+    private Comparator<ListNode> ListNodeComparator = new Comparator<ListNode>(){
+        public int compare(ListNode l1, ListNode l2){
+            if(l1 == null){
                 return 1;
-            }else if(right == null){
-                return -1;// -1 代表左边比右边小，所以给左边的node
             }
-            return left.val - right.val;
-            
-            
-            // 在compare里面， -1代表 left<right;   +1 代表 left>right
+            if(l2 == null){
+                return -1;
+            }
+            return l1.val - l2.val;
         }
     };
+ 
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null || lists.length == 0) {
-            return null;
-        }
-        
-        Queue<ListNode> heap = new PriorityQueue<ListNode>(lists.length, ListNodeComparator);
-        for(int i = 0; i < lists.length; i++){
-           if (lists[i] != null) {
-                heap.add(lists[i]);
-            }
-        }
-        
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        while(!heap.isEmpty()){
-            ListNode head = heap.poll();
-            tail.next = head; 
-            tail = head; //要移动 tail 指针
-            if (head.next != null) {
-                heap.add(head.next);
-            }
-        }
-        
-        return dummy.next;
-
+       PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.length, ListNodeComparator);
+       for(int i = 0; i < lists.length; i++){
+           if(lists[i] != null){
+               pq.add(lists[i]);
+           }
+       }
+       ListNode dummy = new ListNode(0);
+       ListNode cur = dummy;
+       while(!pq.isEmpty()){
+           ListNode tmp = pq.poll();
+           cur.next = tmp;
+           cur = cur.next;
+           if(tmp.next != null){
+               pq.add(tmp.next);
+           }
+       }
+       return dummy.next;
     }
 }
