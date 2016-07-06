0ae9a30407f06bf1fcee6d07232f6f18fdd00b4a
seaskymonster <wenlaizhang@gmail.com>
2016-07-06 01:25:36 -0700

    [94. Binary Tree Inorder Traversal][Accepted]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -8,22 +8,40 @@
  * }
  */
 public class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        if(root == null) return result;
-        TreeNode cur = root;
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> result = new ArrayList<Integer>();
+    //     Stack<TreeNode> stack = new Stack<TreeNode>();
+    //     if(root == null) return result;
+    //     TreeNode cur = root;
 
-        while(!stack.isEmtpy()){
-            while(cur.left != null){
-                stack.push(cur);
-                cur = cur.left;
+    //     while(!stack.empty()){
+    //         while(cur != null){
+    //             stack.add(cur);
+    //             cur = cur.left;
+    //         }
+    //         cur = stack.peek();
+    //         stack.pop();
+    //         result.add(cur.val);
+    //         if(cur.right != null){
+    //             cur = cur.right;
+    //         }
+    //     }
+    //     return result;
+    // }
+    
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        TreeNode curt = root;
+        while (curt != null || !stack.empty()) {
+            while (curt != null) {
+                stack.add(curt);
+                curt = curt.left;
             }
-            cur = stack.pop();
-            result.add(cur.val);
-            if(cur.right != null){
-                cur = cur.right;
-            }
+            curt = stack.peek();
+            stack.pop();
+            result.add(curt.val);
+            curt = curt.right;
         }
         return result;
     }
