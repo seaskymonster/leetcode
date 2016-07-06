b412e310033ba0cbfcd83e9956b5b596525ecda8
seaskymonster <wenlaizhang@gmail.com>
2016-07-05 23:40:42 -0700

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -10,11 +10,17 @@
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        if(root.left == null && root.right == null) return true;
-        if(root.left != null && root.right != null){
-            return (root.left.val == root.right.val && isSymmetirc(root.left) && isSymmetric(root.right));
+        return isMirror(root, root);
+    }
+    
+    public boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null) {
+            return true;
+        }
+        if(left == null || right == null) {
+            return false;
         }
         
-        return false;
+        return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
 }
