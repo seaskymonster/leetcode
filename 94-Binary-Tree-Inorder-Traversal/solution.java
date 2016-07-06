e925b23434f94636cf20d9acea10dca6f157d908
seaskymonster <wenlaizhang@gmail.com>
2016-07-06 01:26:16 -0700

    [94. Binary Tree Inorder Traversal][Time Limit Exceeded]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -8,41 +8,41 @@
  * }
  */
 public class Solution {
-    // public List<Integer> inorderTraversal(TreeNode root) {
-    //     List<Integer> result = new ArrayList<Integer>();
-    //     Stack<TreeNode> stack = new Stack<TreeNode>();
-    //     if(root == null) return result;
-    //     TreeNode cur = root;
-
-    //     while(!stack.empty()){
-    //         while(cur != null){
-    //             stack.add(cur);
-    //             cur = cur.left;
-    //         }
-    //         cur = stack.peek();
-    //         stack.pop();
-    //         result.add(cur.val);
-    //         if(cur.right != null){
-    //             cur = cur.right;
-    //         }
-    //     }
-    //     return result;
-    // }
-    
-    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        TreeNode curt = root;
-        while (curt != null || !stack.empty()) {
-            while (curt != null) {
-                stack.add(curt);
-                curt = curt.left;
+        if(root == null) return result;
+        TreeNode cur = root;
+
+        while(!stack.empty() || cur != null){
+            while(cur != null){
+                stack.add(cur);
+                cur = cur.left;
             }
-            curt = stack.peek();
+            cur = stack.peek();
             stack.pop();
-            result.add(curt.val);
-            curt = curt.right;
+            result.add(cur.val);
+            if(cur.right != null){
+                cur = cur.right;
+            }
         }
         return result;
     }
+    
+    // public ArrayList<Integer> inorderTraversal(TreeNode root) {
+    //     Stack<TreeNode> stack = new Stack<TreeNode>();
+    //     ArrayList<Integer> result = new ArrayList<Integer>();
+    //     TreeNode curt = root;
+    //     while (curt != null || !stack.empty()) {
+    //         while (curt != null) {
+    //             stack.add(curt);
+    //             curt = curt.left;
+    //         }
+    //         curt = stack.peek();
+    //         stack.pop();
+    //         result.add(curt.val);
+    //         curt = curt.right;
+    //     }
+    //     return result;
+    // }
 }
