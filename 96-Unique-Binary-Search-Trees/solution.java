55150b89fcd86d4a5ffa06bd9be7cb7f37eef1fa
seaskymonster <wenlaizhang@gmail.com>
2016-07-05 23:20:16 -0700

    [96. Unique Binary Search Trees][Compile Error]committed by LeetCode Extension

---
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int numTrees(int n) {
+        int[]  f= new int[n+1];
+        f[0] = 1;
+        f[1] = 1;
+        for(int i = 2; i <= n; i++){
+            for(int j = 0; j < i; j++){
+                f[i] += f[j] * f[i-1-j];
+            }
+        }
+        return f[n]
+    }
+}
