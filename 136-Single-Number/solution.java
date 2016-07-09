07fb85fe839695d1847557719d719b087b7f5bc2
seaskymonster <wenlaizhang@gmail.com>
2016-07-09 02:22:49 -0700

    [136. Single Number][Compile Error]committed by LeetCode Extension

--- 136-Single-Number/solution.java
+++ 136-Single-Number/solution.java
@@ -1,11 +1,8 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length == 0) {
-            return 0;
-        }
-        int result = 0;
+        int result  = 0;
         for(int i = 0; i < nums.length; i++){
-            result = result ^ nums[i];
+            result = result xor nums[i];
         }
         return result;
     }
