d1115ea79a7c0cbb77b74d311307649620a7ce2b
seaskymonster <wenlaizhang@gmail.com>
2016-07-16 16:36:59 -0700

    [47. Permutations II][Accepted]committed by LeetCode Extension

--- 47-Permutations-II/solution.java
+++ 47-Permutations-II/solution.java
@@ -12,16 +12,20 @@ public class Solution {
     }
     
     public void helper(List<List<Integer>> result, List<Integer> list, int[] visit, int[] nums){
-        if(list.size() == nums.size()){
+        if(list.size() == nums.length){
             result.add(new ArrayList(list));
+            return;
         }
         for(int i = 0; i < nums.length; i++){
-            if(i != 0 && nums[i] == nums[i-1]){
+            if(visit[i] == 1 || (i != 0 && nums[i] == nums[i-1] && visit[i-1] == 0)){
+                // 没明白为啥要加最后 那个visit[i-1] == 0
                 continue;
             }
+            visit[i] = 1;
             list.add(nums[i]);
             helper(result, list, visit, nums);
             list.remove(list.size()-1);
+            visit[i] = 0;
         }
     }
     
