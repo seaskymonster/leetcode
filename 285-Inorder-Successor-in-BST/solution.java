0e70018b2813a8bc3f1ccd205833451f4fb5271b
seaskymonster <wenlaizhang@gmail.com>
2016-07-18 21:09:25 -0700

    [285. Inorder Successor in BST][Runtime Error]committed by LeetCode Extension

--- 285-Inorder-Successor-in-BST/solution.java
+++ 285-Inorder-Successor-in-BST/solution.java
@@ -8,26 +8,55 @@
  * }
  */
 public class Solution {
-    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode cur = root;
-        while(!stack.isEmpty() || cur != null){
-            while(cur != null){
-                stack.push(cur);
-                cur = cur.left;
-            }
-            TreeNode node = stack.pop();
-            if(node == p){
-                cur = node.right;
-                if(cur != null){
-                    return cur;
-                }else if(!stack.isEmpty){
-                    return stack.pop();
-                }
-            }
-            cur = node.right;
+    // public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    //     Stack<TreeNode> stack = new Stack<>();
+    //     TreeNode cur = root;
+    //     while(!stack.isEmpty() || cur != null){
+    //         while(cur != null){
+    //             stack.push(cur);
+    //             cur = cur.left;
+    //         }
+    //         TreeNode node = stack.pop();
+    //         if(node == p){
+    //             cur = node.right;
+    //             if(cur != null){
+    //                 return cur;
+    //             }else if(!stack.isEmpty()){
+    //                 return stack.pop();
+    //             }
+    //             break;
+    //         }
+    //         cur = node.right;
             
-        }
-      return null;
+    //     }
+    //   return null;
+    // }
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+       TreeNode cur = root;
+       TreeNode successor = null;
+       while(cur != null || cur.val == p.val){
+           if(cur.val > p.val){
+               successor = cur;
+               cur = cur.left;
+           }else{
+               cur = cur.right;
+           }
+       }
+       
+       if(cur == null){
+           return null;
+       }
+       
+       if(cur.right == null){
+           return successor;
+       }
+       
+       cur = cur.right;
+       while(cur.left != null){
+           cur = cur.left;
+       }
+       
+       return cur;
     }
 }
