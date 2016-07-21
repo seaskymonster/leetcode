6d9a7e42e417d4dd0280a30bbd4f865c26ce4d3c
seaskymonster <wenlaizhang@gmail.com>
2016-07-20 21:13:06 -0700

    [223. Rectangle Area][Accepted]committed by LeetCode Extension

---
+++ 223-Rectangle-Area/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int area1 = (C - A)*(D - B);
+        int area2 = (G - E)*(H - F);
+        int leftx = Math.max(A,E);
+        int lefty = Math.max(B,F);
+        int rightx = Math.max(Math.min(C,G), leftx);
+        int righty = Math.max(Math.min(D,H), lefty);
+        
+        return area1 + area2 - (rightx - leftx)*(righty - lefty);
+        
+    }
+}
