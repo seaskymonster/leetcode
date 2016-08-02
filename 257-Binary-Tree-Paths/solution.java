05cedfbe3e8d99922fb49f4558a8b75735ee98d3
seaskymonster <wenlaizhang@gmail.com>
2016-08-01 20:20:58 -0700

    [257. Binary Tree Paths][Accepted]committed by LeetCode Extension

--- 257-Binary-Tree-Paths/solution.java
+++ 257-Binary-Tree-Paths/solution.java
@@ -14,20 +14,22 @@ public class Solution {
           return result;
       }
       String s = new String();
-      branch(result, root, s);
+      branch(result, root, s + root.val);
+      return result;
     }
     
     public void branch(List<String> result, TreeNode root, String s){
+      
+        
         if(root.left == null && root.right == null){
-            s = s + "->" +root.val
             result.add(new String(s));
             return;
         }
         if(root.left != null){
-           branch(result, root.left, s+ "->" + root.val)
+           branch(result, root.left, s+ "->" + root.left.val); //iterative, 即使要跟17行一样。。。把要遍历点的值加进去。
         }
-        if(root.rigth != null){
-           branch(result, root.right, s+ "->" + root.val)
+        if(root.right != null){
+           branch(result, root.right, s+ "->" + root.right.val);
         }
     }
 }
