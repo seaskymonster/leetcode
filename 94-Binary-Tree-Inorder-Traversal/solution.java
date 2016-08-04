8543cb0b18a167bd807dd28ae20f2d186f143bb7
seaskymonster <wenlaizhang@gmail.com>
2016-08-03 20:31:38 -0700

    [94. Binary Tree Inorder Traversal][Accepted]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -12,37 +12,17 @@ public class Solution {
         List<Integer> result = new ArrayList<Integer>();
         Stack<TreeNode> stack = new Stack<TreeNode>();
         if(root == null) return result;
+        //stack.push(root);
         TreeNode cur = root;
-
-        while(!stack.empty() || cur != null){ // 这里一定加 || cur != null, 说明当前节点还没遍历完。即使现在stack是空的，我还有当前节点的自己点可以往stack 里面放。
+        while(!stack.isEmpty() || cur != null){
             while(cur != null){
-                stack.add(cur);
+                stack.push(cur);
                 cur = cur.left;
             }
-            cur = stack.peek();
-            stack.pop();
+            cur = stack.pop();
             result.add(cur.val);
-            // if(cur.right != null){ // 没想明白为啥不能加这句。
-                cur = cur.right;
-            
+            cur = cur.right;
         }
         return result;
     }
-    
-    // public ArrayList<Integer> inorderTraversal(TreeNode root) {
-    //     Stack<TreeNode> stack = new Stack<TreeNode>();
-    //     ArrayList<Integer> result = new ArrayList<Integer>();
-    //     TreeNode curt = root;
-    //     while (curt != null || !stack.empty()) {
-    //         while (curt != null) {
-    //             stack.add(curt);
-    //             curt = curt.left;
-    //         }
-    //         curt = stack.peek();
-    //         stack.pop();
-    //         result.add(curt.val);
-    //         curt = curt.right;
-    //     }
-    //     return result;
-    // }
 }
