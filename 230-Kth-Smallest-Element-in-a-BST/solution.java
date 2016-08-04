f72ed569a866d548d5d5485288d204a5985eef5f
seaskymonster <wenlaizhang@gmail.com>
2016-08-04 11:48:54 -0700

    [230. Kth Smallest Element in a BST][Runtime Error]committed by LeetCode Extension

---
+++ 230-Kth-Smallest-Element-in-a-BST/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root == null) return -1;
+        TreeNode cur = root;
+        while(!stack.isEmpty() || cur != null){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            TreeNode node = stack.pop();
+            k--;
+            if(k == 0){
+                return node.val;
+            }
+            cur = cur.right;
+        }
+        return -1;
+    }
+}
