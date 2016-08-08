fa14e09a85fc09b3734269c66c3a73227e944544
seaskymonster <wenlaizhang@gmail.com>
2016-08-08 00:33:48 -0700

    [208. Implement Trie (Prefix Tree)][Compile Error]committed by LeetCode Extension

---
+++ 208-Implement-Trie-(Prefix-Tree)/solution.java
@@ -0,0 +1,66 @@
+class TrieNode {
+    // Initialize your data structure here.
+    private TrieNode[] children;
+    public boolean hasWord;
+    
+    public TrieNode() {
+        children = new TrieNode[26];
+        hasWord = false;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode cur = root;
+        for(int i = 0; i < word.length; i++){
+            Character tmp = word.charAt(i);
+            if(cur.children[tmp - 'a'] == null){
+                cur.children[tmp - 'a'] = new TrieNode();
+                cur = cur.children[tmp - 'a'];
+            }
+        }
+        cur.hasWrod = true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for(int i = 0; i < word.length; i++){
+            Character tmp = word.charAt(i);
+            if(cur.children[tmp - 'a'] == null){
+              return null;
+            }else{
+             cur = cur.children[tmp - 'a'];
+            }
+        }
+        return cur.hasWord;
+        
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+         TrieNode cur = root;
+        for(int i = 0; i < prefix.length; i++){
+            Character tmp = word.charAt(i);
+            if(cur.children[tmp - 'a'] == null){
+              return null;
+            }else{
+             cur = cur.children[tmp - 'a'];
+            }
+        }
+        return true;
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
