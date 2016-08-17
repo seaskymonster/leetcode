3402a01078787ab483c3161199299d08745c7cba
seaskymonster <wenlaizhang@gmail.com>
2016-08-16 23:26:06 -0700

    [22. Generate Parentheses][Compile Error]committed by LeetCode Extension

---
+++ 22-Generate-Parentheses/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        helper(result, "", n, n);
+        return result;
+    }
+    
+    public helper(List<String> result, String s, int left, int right){
+        if(left > right) return;
+        if(left == 0 && right == 0){
+            result.add(s);
+            return;
+        }
+        if(left > 0){
+            helper(result, s+"(", left-1, right);
+        }
+        if(right > 0){
+            helper(result, s+")", left, right -1);
+        }
+    }
+}
