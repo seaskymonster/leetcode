6c36f247a148572797ebffaebbb020f3517ff420
seaskymonster <wenlaizhang@gmail.com>
2016-08-17 20:55:31 -0700

    [200. Number of Islands][Runtime Error]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,32 +1,64 @@
 public class Solution {
+    public int convertToId(int x, int y, int n){
+        return n*x + y;
+    }
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) {
-            return 0;
-        }
         int m = grid.length;
         int n = grid[0].length;
+        UF uf = new UF(m, n);
         int count = 0;
-        for(int i = 0; i < m; i++){
+        for(int i =0 ; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid, i, j);
-                    count++;
+                if(grid[i][j] == '0'){
+                    continue;
+                }else{
+                   if(i > 0 && grid[i-1][j] == '1'){
+                       uf.union(convertToId(i, j, n), convertToId(i-1, j, n));
+                   }else if(j > 0 && grid[i][j-1] == '1'){
+                       uf.union(convertToId(i, j, n), convertToId(i, j-1, n));
+                   }else{
+                       count++;
+                   }
+                   
                 }
             }
         }
         return count;
     }
     
-    public void dfs(char[][] grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length){
-            return;
+    class UF{
+        HashMap<Integer, Integer> father = new HashMap<Integer, Integer>();
+        
+        UF(int m, int n){
+           for(int i = 0; i < m; i++){
+               for(int j = 0; j < n; j++){
+                   int id = convertToId(i, j ,n);
+                   father.put(id, id); // 初始化。
+               }
+           } 
         }
-        if(grid[i][j] == '1'){
-            grid[i][j] = 0;
-            dfs(grid, i-1, j);
-            dfs(grid, i+1, j);
-            dfs(grid, i, j-1);
-            dfs(grid, i, j+1);
+        
+        public int compressed_find(int x){
+            int parent = x;
+            while(parent != father.get(parent)){
+                parent = father.get(parent);
+            }
+            int tmp = -1;
+            int fa = x;
+            while(fa != father.get(fa)){
+                tmp = father.get(fa);
+                father.put(fa, parent);
+                fa = tmp;
+            }
+            return parent;
+        }
+        
+        public void union(int x, int y){
+            int pr_x = compressed_find(x);
+            int pr_y = compressed_find(y);
+            if(pr_x != pr_y){
+                father.put(pr_x, pr_y);
+            }
         }
     }
 }
