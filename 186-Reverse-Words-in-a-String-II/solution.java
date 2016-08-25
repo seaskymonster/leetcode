ff05a9d29cd4f002cdfd9daa50eee92c1d2736db
seaskymonster <wenlaizhang@gmail.com>
2016-08-24 17:35:07 -0700

    [186. Reverse Words in a String II][Compile Error]committed by LeetCode Extension

--- 186-Reverse-Words-in-a-String-II/solution.java
+++ 186-Reverse-Words-in-a-String-II/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public void reverseWords(char[] s) {
         int i = 0;
         int j = 0;
-        while(j < s.lenght){
+        while(j < s.length){
             while(j < s.length && s[j] != ' '){
                 j++;
             }
