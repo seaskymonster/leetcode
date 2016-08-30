7f41c5145445fe8c5a8ed2ab358bc35fe603980e
seaskymonster <wenlaizhang@gmail.com>
2016-08-29 17:33:42 -0700

    [387. First Unique Character in a String][Compile Error]committed by LeetCode Extension

---
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int firstUniqChar(String s) {
+        int[] count = new int[128];
+        for(int i = 0; i < s.length(); i++){
+            count[charAt(i)-'a']++;
+        }
+        for(int j =0; j<s.length(); j++){
+            if(count[charAt(j)-'a'] == 1){
+              return j;
+            }
+        }
+        return -1;
+    }
+}
