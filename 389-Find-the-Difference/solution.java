c2007d198684e43f4e0ce88631af5fca8a392f07
seaskymonster <wenlaizhang@gmail.com>
2016-08-29 17:42:34 -0700

    [389. Find the Difference][Runtime Error]committed by LeetCode Extension

--- 389-Find-the-Difference/solution.java
+++ 389-Find-the-Difference/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public char findTheDifference(String s, String t) {
         int[] count = new int[128];
-        for(int i = 0; i < t.length(); i++){
+        for(int i = 0; i < s.length(); i++){
             count[t.charAt(i) - 'a']++;
             count[s.charAt(i) - 'a']--;
         }
