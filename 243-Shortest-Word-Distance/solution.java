077cb9ab2db5af101788f9a0384f4d85bcf168cc
seaskymonster <wenlaizhang@gmail.com>
2016-09-07 01:10:18 -0700

    [243. Shortest Word Distance][Accepted]committed by LeetCode Extension

--- 243-Shortest-Word-Distance/solution.java
+++ 243-Shortest-Word-Distance/solution.java
@@ -1,19 +1,20 @@
 public class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-       int i1 = -1;
-       int i2 = -1;
-       int shortest = Integer.MAX_VALUE;
-       for(int i = 0; i < words.length; i++){
-           if(words[i].equals(word1)){
-               i1 = i;
-           }else if(words[i].equals(word2)){
-               i2 = i;
-           }
-           
-           if(i1 != -1 && i2 != -1){
-               shortest = Math.min(shortest, Math.abs(i1 - i2));
-           }
-       }
-       return shortest;
+        int pos1 = -1;
+        int pos2 = -1;
+        int result = Integer.MAX_VALUE;
+        for(int i = 0; i < words.length; i++){
+            if(words[i].equals(word1)){
+                pos1 = i;
+            }
+            if(words[i].equals(word2)){
+                pos2 = i;
+            }
+            if(pos1 != -1 && pos2 != -1){
+                result = Math.min(result, Math.abs(pos1 - pos2));
+            }
+        }
+        result = result == Integer.MAX_VALUE ? -1: result;
+        return result;
     }
 }
