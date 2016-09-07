643475f4dcea81844c6892e04423ac871eaf9cbf
seaskymonster <wenlaizhang@gmail.com>
2016-09-07 01:33:26 -0700

    [240. Search a 2D Matrix II][Compile Error]committed by LeetCode Extension

---
+++ 240-Search-a-2D-Matrix-II/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n= matrix[0].length;
+        int i = m-1;
+        int j =0;
+        while( i>= 0 && i < m && j <n && j >= 0){
+            if(matrix[i][j] > target){
+               i--; 
+            }else if (matrix[i][j] < target){
+               j++;
+            }else{
+                return true;
+            }
+        }
+    }
+}
