2c442a94d28234e33d24bd9bbf60598ed6f6c8c5
seaskymonster <wenlaizhang@gmail.com>
2016-09-08 02:29:59 -0700

    [7. Reverse Integer][Accepted]committed by LeetCode Extension

--- 7-Reverse-Integer/solution.java
+++ 7-Reverse-Integer/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int reverse(int x) {
         long result = 0;
-        while(x != 0){
-            result = result*10 + x%10;
+        while(x >=1 || x <= -1){
+            result = result * 10 + x%10;
             x = x/10;
         }
-        if(x < Integer.MIN_VALUE || x > Integer.MAX_VALUE){
-            return 0;
-        }
-        return int(result);
+       if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+        return 0;
+       }
+        return (int)result;
     }
 }
