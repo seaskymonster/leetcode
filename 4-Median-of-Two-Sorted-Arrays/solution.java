eb62af8e4c76b0ff69f9aeaadbd3ed5eb4ef846d
seaskymonster <wenlaizhang@gmail.com>
2016-09-08 02:15:09 -0700

    [4. Median of Two Sorted Arrays][Runtime Error]committed by LeetCode Extension

--- 4-Median-of-Two-Sorted-Arrays/solution.java
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -2,28 +2,28 @@ public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int length = nums1.length + nums2.length;
         if(length%2 == 1){
-           return findfortheKth(nums1, nums2, length/2+1, 0, nums1.length-1, 0, nums2.length-1);
+           return findfortheKth(nums1, nums2, length/2+1, 0, nums1.length-1, 0, nums2.length-1); // 如果总共有3个，找第2大
         }else{
           return (findfortheKth(nums1, nums2, length/2, 0, nums1.length-1, 0, nums2.length-1)
-                 +findfortheKth(nums1, nums2, length/2+1, 0, nums2.length-1, 0, nums2.length-1))/2;
+                 +findfortheKth(nums1, nums2, length/2+1, 0, nums2.length-1, 0, nums2.length-1))/2; //如果总共有4个，找第2大，和第3大。
 
         }
     }
     
     
-    public int findfortheKth(int[] nums1, int[] nums2, int k, int s1, int e1, int s2, int e2){
-        if(s1 >= e1){
+    public int findfortheKth(int[] nums1, int[] nums2, int k, int s1, int e1, int s2, int e2){ // 找第k大。
+        if(s1 > e1){
             return nums2[s2+k-1];
         }
-        if(s2 >= e2) {
+        if(s2 > e2) {
             return nums1[s1+k-1];
         }
         if(k == 1){
             return Math.min(nums1[s1], nums2[s2]);
         }
          
-        int A_key = s1 + k/2 - 1 >= e1 ? nums1[s1 + k/2 -1]: Integer.MAX_VALUE;
-        int B_key = s2 + k/2 - 1 >= e2 ? nums2[s2 + k/2 -1]: Integer.MAX_VALUE;
+        int A_key = s1 + k/2 - 1 <= e1 ? nums1[s1 + k/2 -1]: Integer.MAX_VALUE;
+        int B_key = s2 + k/2 - 1 <= e2 ? nums2[s2 + k/2 -1]: Integer.MAX_VALUE;
         if(A_key > B_key){
            return findfortheKth(nums1, nums2, k-k/2, s1, e1, s2+k/2, e2);
         }else{
