b688e706a268c6e2164681696c96a25fd123978d
seaskymonster <wenlaizhang@gmail.com>
2016-09-10 02:29:40 -0700

    [322. Coin Change][Time Limit Exceeded]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -5,8 +5,14 @@ public class Solution {
         f[0] = 0;
         for(int i = 1 ; i <= amount; i++){
             for(int j =0; j < coins.length; j++){
-                if(i- conis[j] >= 0 && f[i -coins[j]] != -1){
-                    f[i] = Math.min(f[i - coins[j]] +1, f[i]);
+                System.out.println('i'+i);
+                System.out.println('j'+j);
+                if(i- coins[j] >= 0 && f[i -coins[j]] != -1){
+                    if(f[i] == -1){
+                        f[i] = f[i - coins[j]] + 1;
+                    }else{
+                        f[i] = Math.min(f[i - coins[j]] +1, f[i]);
+                    }
                 }  
             }
         }
