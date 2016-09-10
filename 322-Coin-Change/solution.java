cefe11ccc4e0cac6af558b46aed6d29c4d089688
seaskymonster <wenlaizhang@gmail.com>
2016-09-10 01:40:04 -0700

    [322. Coin Change][Runtime Error]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,36 +1,15 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] f = new int[amount+1]; // f[i] 代表 i这么多钱最少是由多少个coins组成的。  
-       
-        Arrays.fill(f, Integer.MAX_VALUE);
+        int[] f = new int[amount +1];
+        Arrays.fill(f, -1);
         f[0] = 0;
-        for(int i = 1; i < f.length; i++){
-            for(int j = 0; j < coins.length; j++){
-                if(i - coins[j] >= 0){
-                    if(f[i-coins[j]] != Integer.MAX_VALUE){
-                          f[i] = Math.min(f[i], f[i-coins[j]] + 1);
-                    }
-                }
+        for(int i = 1 ; i <= amount; i++){
+            for(int j =0; j < coins.length; j++){
+                if(f[i -coins[j]] != -1){
+                    f[i] = Math.min(f[i - coins[j]] +1, f[i]);
+                }  
             }
         }
-        return f[amount] == Integer.MAX_VALUE ? -1 : f[amount];
+        return f[amount];
     }
 }
-
-// public class Solution {
-//     public int coinChange(int[] coins, int amount) {
-//         int max = amount + 1;             
-//         int[] dp = new int[amount + 1];  
-//         Arrays.fill(dp, max);  
-//         dp[0] = 0;   
-//         for (int i = 1; i <= amount; i++) {
-//             for (int j = 0; j < coins.length; j++) {
-//                 if (coins[j] <= i) {
-//                     dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-//                 }
-//             }
-//         }
-//         return dp[amount] > amount ? -1 : dp[amount];
-//     }
-// }
-
