e1595dce506050cffc443f89c0a8459f789041fc
seaskymonster <wenlaizhang@gmail.com>
2016-09-11 15:49:02 -0700

    [258. Add Digits][Accepted]committed by LeetCode Extension

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,15 +1,13 @@
 public class Solution {
-    public int addDigits(int num) {  // recursive call the sumDigits function.
-       while(num/10 >= 1){
-           num = sumDigits(num);
-       }
-       return num;
-    }
-    
-    public int sumDigits(int num) {
-       if(num < 10){
-           return num;
-       }
-       return num%10 + addDigits(num/10);
+    public int addDigits(int num) {
+        while(num >=10){
+            int result = 0;
+            while(num >= 1){
+                result = result + num%10;
+                num = num/10;
+            }
+            num = result;
+        }
+        return num;
     }
 }
