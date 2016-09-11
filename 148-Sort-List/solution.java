b879e25bc5ed57193c62833ad1d8a603b70b8afc
seaskymonster <wenlaizhang@gmail.com>
2016-09-11 15:38:09 -0700

    [148. Sort List][Compile Error]committed by LeetCode Extension

--- 148-Sort-List/solution.java
+++ 148-Sort-List/solution.java
@@ -8,49 +8,46 @@
  */
 public class Solution {
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
-        }
-        ListNode mid = findMidNode(head);
-        ListNode right = sortList(mid.next);
+        if(head == null || head.next == null) return head;
+        ListNode mid = findMid(head);
+        ListNode right = mid.next;
         mid.next = null;
         ListNode left = sortList(head);
-        return merge(left, right);
-
+        ListNode right = sortList(right);
+        ListNode result = merge(left, right);
+        return result;
+        
     }
     
     public ListNode merge(ListNode left, ListNode right){
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+        ListNode cur = dummy;
         while(left != null && right != null){
-            if(left.val < right.val){
-                tail.next = left;
-                left = left.next;
-            }else{
-                tail.next = right;
+            if(left.val >= right.val){
+                cur.next = right;
                 right = right.next;
+            }else{
+                cur.next = left;
+                left = left.next;
             }
-            tail = tail.next;
+            cur = cur.next;
         }
         if(left != null){
-            tail.next = left;
+            cur.next = left;
         }
         if(right != null){
-            tail.next = right;
+            cur.next = right;
         }
         return dummy.next;
     }
     
-    
-    public ListNode findMidNode(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
-        }
+    public ListNode findMid(ListNode head) {
+        if(head == null || head.next == null) return head;
         ListNode slow = head;
         ListNode fast = head.next;
         while(fast != null && fast.next != null){
-            slow = slow.next;
             fast = fast.next.next;
+            slow = slow.next;
         }
         return slow;
     }
