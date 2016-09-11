9faa58df1cf6a33403498bc8cf4e666564b72ada
seaskymonster <wenlaizhang@gmail.com>
2016-09-11 03:36:24 -0700

    [206. Reverse Linked List][Compile Error]committed by LeetCode Extension

--- 206-Reverse-Linked-List/solution.java
+++ 206-Reverse-Linked-List/solution.java
@@ -7,14 +7,30 @@
  * }
  */
 public class Solution {
-    public ListNode reverseList(ListNode head) {
-       ListNode pre = null;
-       while(head != null){
-           ListNode tmp = head.next;
-           head.next = pre;
-           pre = head;
-           head= tmp;
+    // public ListNode reverseList(ListNode head) {
+    //   ListNode pre = null;
+    //   while(head != null){
+    //       ListNode tmp = head.next;
+    //       head.next = pre;
+    //       pre = head;
+    //       head= tmp;
+    //   }
+    //   return pre;
+    // }
+    
+       public ListNode reverseList(ListNode head) {
+           // if head is null or only one node, it's reverse of itself.
+  if ( (head==null) || (head.next == null) ) return head;
+
+  // reverse the sub-list leaving the head node.
+  Node reverse = reverse(head.next);
+
+  // head.next still points to the last element of reversed sub-list.
+  // so move the head to end.
+  head.next.next = head;
+
+  // point last node to nil, (get rid of cycles)
+  head.next = null;
+  return reverse;
        }
-       return pre;
-    }
 }
