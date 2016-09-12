6dbd7e76f33596011a38d0e78259b8b1bc76c474
seaskymonster <wenlaizhang@gmail.com>
2016-09-12 00:44:10 -0700

    [150. Evaluate Reverse Polish Notation][Accepted]committed by LeetCode Extension

--- 150-Evaluate-Reverse-Polish-Notation/solution.java
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         Stack<Integer> stack = new Stack<Integer>();
         for(String str: tokens){
             if(!(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/"))){
-                stack.push(str);
+                stack.push(Integer.valueOf(str));
             }else{
                 int a = stack.pop();
                 int b = stack.pop();
