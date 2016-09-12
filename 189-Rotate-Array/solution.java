372dc992c0d3692cce86634a31874d3bd6a2103b
seaskymonster <wenlaizhang@gmail.com>
2016-09-11 23:36:17 -0700

    [189. Rotate Array][Compile Error]committed by LeetCode Extension

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,19 +1,19 @@
 public class Solution {
     public void rotate(int[] nums, int k) {
-        k = k%nums.length;
-        reverse(nums, 0, nums.length-1);
-        reverse(nums, 0, k-1);
-        reverse(nums, k, nums.length-1);
-        
+        k = k%n;
+        reverse(nums, 0, n-k-1);
+        reverse(nums, n-k, n-1);
+        return;
     }
     
-    public void reverse(int[] nums, int from, int to){
-        while(from < to){
-            int tmp = nums[from];
-            nums[from] = nums[to];
-            nums[to] = tmp;
-            from++;
-            to--;
+    public void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = nums[start];
+            start++;
+            end--;
         }
+        return;
     }
 }
