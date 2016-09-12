c1b709ae9c8224b75e599dbc6669b07411990a69
seaskymonster <wenlaizhang@gmail.com>
2016-09-12 00:35:20 -0700

    [67. Add Binary][Compile Error]committed by LeetCode Extension

---
+++ 67-Add-Binary/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public String addBinary(String a, String b) {
+        if(a.length() < b.length()){
+            String tmp = b;
+            b =a;
+            a= tmp;
+        }
+        
+        int pa = a.length()-1;
+        int pb = b.length()-1
+        int carry = 0;
+        String result = "";
+        while(pb>=0){
+            int sum = (int)(a.charAt(pa) - '0')+(b.charAt(pb) -'0')+carry;
+            result = String.valueOf(sum%2) +result;
+            carry = sum/2;
+            pa--;
+            pb--;
+        }
+        while(pa >= 0){
+            int sum = (int)(a.charAt(pa) - '0') + carry;
+            result = String.valueOf(sum%2) +result;
+            carry = sum/2;
+            pa--;
+        }
+        if(carry == 1){
+            result = "1" + result;
+        }
+        return result;
+    }
+}
