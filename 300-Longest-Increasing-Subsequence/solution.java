81b55eb5d7f56b7236aed42b2d24a575fb1eeef1
seaskymonster <wenlaizhang@gmail.com>
2016-09-18 23:52:09 -0700

    [300. Longest Increasing Subsequence][Accepted]committed by LeetCode Extension

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -1,20 +1,17 @@
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] f = new int[nums.length]; // f[i] 代表以i为结尾的之前的最长子序列
-        for(int i = 0; i < nums.length; i++){
-            f[i] = 1;//初始化所有f为1，至少有一个以自己为最长。
-        }
-        
-        for(int i = 1; i < nums.length; i++){
+        int[] dp = new int[nums.length]; // dp[i] represent lengthofLIS at the end of nums[i];
+        Arrays.fill(dp, 1); // for every element, the minum length is 1
+        for(int i = 1; i < dp.length; i++){
             for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                   f[i] = Math.max(f[j]+1, f[i]);
+                if(nums[i] > nums[j]){
+                   dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
         }
         int result = 0;
-        for(int i = 0; i <nums.length; i++){
-            result = Math.max(result, f[i]);
+        for(int i = 0; i< dp.length; i++){
+            result = Math.max(dp[i], result);
         }
         return result;
     }
