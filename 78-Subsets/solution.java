9eccfd2da750944d3ba8598483632590f99f9603
seaskymonster <wenlaizhang@gmail.com>
2016-09-22 22:06:05 -0700

    [78. Subsets][Compile Error]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,22 +1,21 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-      List<List<Integer>> result = new ArrayList<>();
-      if(nums.length == 0) {
-          return result;
-      }
-      List<Integer> path = new ArrayList<Integer>(); // why this need to be initialized ?
-      int pos = 0;
-      helper(nums, pos, path, result);
-      return result;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(result, path, 0, nums);
+        return result;
     }
     
-    public void helper(int[] nums, int pos, List<Integer> path, List<List<Integer>> result){
-        result.add(new ArrayList(path)); // 这个地方一定要用new
+    public void dfs(List<List<Integer>> result, List<Integer> path, int pos, int[] nums){
+        if(pos >= nums.length) return;
+        if(path != null){
+           result.add(new ArrayList<>(path));
+        }
         for(int i = pos; i < nums.length; i++){
             path.add(nums[i]);
-            helper(nums, i+1, path, result);
-            path.remove(path.size()-1);
+            dfs(result, path, pos+1, nums);
+            path.remove(path.length()-1);
         }
     }
-   
 }
