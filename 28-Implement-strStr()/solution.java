0be3eed041a6958c4688fc667c2d1456cb9f1701
seaskymonster <wenlaizhang@gmail.com>
2016-09-22 21:38:26 -0700

    [28. Implement strStr()][Time Limit Exceeded]committed by LeetCode Extension

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int strStr(String haystack, String needle) {
         if(needle.length() == 0) return 0;
+        
         for(int i =0 ;i < haystack.length(); i++){
            int j = 0;
            int k = i;
