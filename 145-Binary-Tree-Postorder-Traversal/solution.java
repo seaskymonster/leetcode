15b92cb9ccecd5b311a30da15bbc842726f57281
seaskymonster <wenlaizhang@gmail.com>
2016-09-24 13:44:50 -0700

    [145. Binary Tree Postorder Traversal][Compile Error]committed by LeetCode Extension

---
+++ 145-Binary-Tree-Postorder-Traversal/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>(); 
+        TreeNode cur = root;
+        while(!stack.isEmpty() || cur!=null){
+            while(cur != null){
+                result.addFirst(cur.val);
+                cur = cur.right;
+            }
+            TreeNode tmp = stack.pop();
+            cur = tmp.left;
+        }
+    }
+}
