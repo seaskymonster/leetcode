3ee021b4d4d547abef5b2779429c5825a9eb506a
seaskymonster <wenlaizhang@gmail.com>
2016-09-24 23:42:49 -0700

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,46 +1,42 @@
 public class Solution {
   
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        int[] indegree = new int[numCourses];
-        for(int[] pre : prerequisites){
-            int ready = pre[0]; // the current node
-            int prev = pre[1]; // the previous node
-            indegree[ready]++;
-            if(map.containsKey(prev)){
-                map.get(prev).add(ready);
-            }else{
-                ArrayList<Integer> readylist = new ArrayList<>();
-                readylist.add(ready);
-                map.put(prev, readylist);
-            }
-        }
-        
-        
-        
-        
-        Queue<Integer> q = new LinkedList<Integer>();
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        for(int i =0; i < numCourses; i++){
-            if(indegree[i] == 0){
-                q.offer(i);
-                result.add(i);
-            }
-        }
-
-        while(!q.isEmpty()){
-            int tmp = q.poll();
-
-            if(map.containsKey(tmp)){
-                for(int j: map.get(tmp)){ // map.get(tmp) 返回的是 所有的 下一个节点
-                    indegree[j]--;
-                    if(indegree[j] == 0){
-                         q.offer(j);
-                         result.add(j);
-                     }
-                }
-            }
-        }
-        return result.size() == numCourses;
+       HashMap<Integer, ArrayList<Integer>> map = new HashMap<>(); // store every prev's later couse
+       int[] indegree = new int[numCourses];
+       for(int[] prereq: prerequisites){
+           int course = prereq[0];
+           int pre = prereq[1];
+           indegree[course]++;
+           if(map.containsKey(pre)){
+               map.get(pre).add(course);
+           }else{
+               ArrayList<Integer> list = new ArrayList<>();
+               list.add(course);
+               map.put(pre, list);
+           }
+       }
+       
+       List<Integer> result = new ArrayList<Integer>();
+       Queue<Integer> q = new LinkedList<>();
+       for(int i = 0; i < indegree.length; i++){
+           if(indegree[i] == 0){
+               result.add(i);
+               q.offer(i);
+           }
+       }
+       
+       while(!q.isEmpty()){
+           int tmp = q.poll();
+           if(map.containsKey(tmp)){
+               for(int j: map.get(tmp)){
+                   indegree[j]--;
+                   if(indegree[j] == 0){
+                       q.offer(j);
+                       result.add(j);
+                   }
+               }
+           }
+       }
+       return result.size() == numCourses;
     }
 }
