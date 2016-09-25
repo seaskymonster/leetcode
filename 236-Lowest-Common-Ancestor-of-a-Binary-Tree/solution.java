d36683932084951d4765e8152187a0bef29a4dc2
seaskymonster <wenlaizhang@gmail.com>
2016-09-24 21:06:13 -0700

    [236. Lowest Common Ancestor of a Binary Tree][Accepted]committed by LeetCode Extension

--- 236-Lowest-Common-Ancestor-of-a-Binary-Tree/solution.java
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree/solution.java
@@ -7,43 +7,14 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- 
- //把树中，所有的treenode都loop一遍，每个点返回的是，p 和 q 的公共祖先，从下往上返回的，所以最近公共祖先会返回到原点。
- // 如果一个点只包含node1，那么他返回 node1， 都不包含，返回null
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || p == null || q == null){
-            return null;
-        }
-        return helper(root, p, q);
-    }
-    
-    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q){
-        if(root == null){
-            return null;
-        }
-        if(root == p){
-            return p;
-        }
-        if(root == q){
-            return q;
-        }
-        
-        TreeNode left = helper(root.left, p, q);
-        TreeNode right = helper(root.right, p, q);
-        
-        if(left != null && right != null){
-            return root;
-        }
-        
-        if(left != null){
-            return left;
-        }
-        
-        if(right != null){
-            return right;
-        }
-        return null;// left and right are null, return null
-        
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) return root;
+        if(left != null) return left;
+        if(right != null) return right;
+        return null;
     }
 }
