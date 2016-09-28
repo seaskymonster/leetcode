0defc7804c7b58cce796bb8a3b77d008492ec64c
seaskymonster <wenlaizhang@gmail.com>
2016-09-27 21:23:53 -0700

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
   
     public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
