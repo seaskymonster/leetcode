26bc47d39cd97400077d7505edb7b2b25e54fe92
seaskymonster <wenlaizhang@gmail.com>
2016-09-29 01:14:27 -0700

    [200. Number of Islands][Runtime Error]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,33 +1,63 @@
 public class Solution {
   
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
         int m = grid.length;
         int n = grid[0].length;
+        // UF uf = new UF(m, n);
         int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
                     count++;
-                    dfs(grid, i ,j);
+                    if(i > 0 && grid[i-1][j] == '1'){
+                        count--;
+                        continue;
+                    }
+                    if(j > 0 && grid[i][j-1] == '1'){
+                        count--;
+                    }
                 }
             }
         }
         return count;
+     
     }
     
-    public void dfs(char[][] grid, int i, int j){
-        int m = grid.length;
-        int n = grid[0].length;
-        if(i < 0 || j < 0 || i >= m || j >= n){
-            return;
+    class UF {
+        HashMap<Integer, Integer> father = new HashMap<>();
+        
+        UF(int m, int n){
+            for(int i = 0; i < m; i++){
+                for(int j =0; j < n; j++){
+                    int value = i*n+j;
+                    father.put(value, value);
+                }
+            }
         }
-        if(grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
+        
+        int find(int x){
+            int parent = x;
+            while( parent != father.get(parent)){
+                parent = father.get(parent);
+            }
+            
+            int tmp  = -1;
+            int fa = x;
+            while( fa != father.get(fa)){
+                tmp = father.get(fa);
+                father.put(fa, parent);
+                fa = tmp;
+            }
+            return parent;
+        }
+        
+        void union (int x, int y){
+            int fx = find(x);
+            int fy = find(y);
+            if(fx != fy){
+                father.put(x, y);
+            }
+        }
     }
        
 }
