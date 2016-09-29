c773d018a731bdf4d2dd32c5c5b5daae77f68586
seaskymonster <wenlaizhang@gmail.com>
2016-09-29 01:25:41 -0700

    [200. Number of Islands][Compile Error]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -2,62 +2,63 @@ public class Solution {
     public int convertToId(int x, int y, int n){
         return n*x + y;
     }
-  
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        if(m == 0) return 0;
-        int n = grid[0].length;
-        if(n == 0) return 0;
-        UF uf = new UF(m, n);
+        if(grid == null || grid.length  == 0 || grid[0].length == 0){
+            return 0;
+        }
         int count = 0;
-        for(int i = 0; i < m; i++){
+        int m = grid.length;
+        int n = grid[0].length;
+        UF uf = new UF(m, n);
+        for(int i =0 ; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == '1'){
-                    count++;
-                    if(i > 0 && grid[i-1][j] == '1'){
-                      if(uf.find (convertToId(i, j, n)) == uf.compressed_find(convertToId(i-1, j, n))){
-                           continue;
-                       }else{
+                if(grid[i][j] == '0'){
+                    continue;
+                }else{
+                   count++;
+                   if(i > 0 && grid[i-1][j] == '1'){
+                      if(uf.compressed_find (convertToId(i, j, n)) == uf.compressed_find(convertToId(i-1, j, n))){
+                          continue;
+                      }else{
                           count--;
                           uf.union(convertToId(i, j, n), convertToId(i-1, j, n));
-                       }
-                    }
-                    if(j > 0 && grid[i][j-1] == '1'){
-                        if(uf.find (convertToId(i, j, n)) == uf.find(convertToId(i, j-1, n))){
-                           continue;
-                       }else{
+                       
+                   }
+                   
+                   if(j > 0 && grid[i][j-1] == '1'){
+                      if(uf.compressed_find (convertToId(i, j, n)) == uf.compressed_find(convertToId(i, j-1, n))){
+                          continue;
+                      }else{
                            count--;
                            uf.union(convertToId(i, j, n), convertToId(i, j-1, n));
-                       }
-                    }
+                       
+                   }
                 }
             }
         }
         return count;
-     
     }
     
-    class UF {
-        HashMap<Integer, Integer> father = new HashMap<>();
-        
+    class UF{
+        HashMap<Integer, Integer> father = new HashMap<Integer, Integer>();
+
         UF(int m, int n){
-            for(int i = 0; i < m; i++){
-                for(int j =0; j < n; j++){
-                    int value = i*n+j;
-                    father.put(value, value);
-                }
-            }
+           for(int i = 0; i < m; i++){
+               for(int j = 0; j < n; j++){
+                   int id = convertToId(i, j ,n);
+                   father.put(id, id); // 初始化。
+               }
+           } 
         }
         
-        int find(int x){
+        public int compressed_find(int x){
             int parent = x;
-            while( parent != father.get(parent)){
+            while(parent != father.get(parent)){
                 parent = father.get(parent);
             }
-            
-            int tmp  = -1;
+            int tmp = -1;
             int fa = x;
-            while( fa != father.get(fa)){
+            while(fa != father.get(fa)){
                 tmp = father.get(fa);
                 father.put(fa, parent);
                 fa = tmp;
@@ -65,13 +66,12 @@ public class Solution {
             return parent;
         }
         
-        void union (int x, int y){
-            int fx = find(x);
-            int fy = find(y);
-            if(fx != fy){
-                father.put(x, y);
+        public void union(int x, int y){
+            int pr_x = compressed_find(x);
+            int pr_y = compressed_find(y);
+            if(pr_x != pr_y){
+                father.put(pr_x, pr_y);
             }
         }
     }
-       
 }
