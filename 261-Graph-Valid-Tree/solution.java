0c6e182e23ace9898876c8d51ad2286ce19ec075
seaskymonster <wenlaizhang@gmail.com>
2016-09-29 02:07:04 -0700

    [261. Graph Valid Tree][Accepted]committed by LeetCode Extension

--- 261-Graph-Valid-Tree/solution.java
+++ 261-Graph-Valid-Tree/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public boolean validTree(int n, int[][] edges) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
         for(int i =0; i < n; i++){
-            map.put(i, new ArrayList<>());
+            map.put(i, new HashSet<>());
         }
         for(int[] edge: edges){
             map.get(edge[0]).add(edge[1]);
             map.get(edge[1]).add(edge[0]);
         }
         
-        int[] visited = new int[n];
+        boolean[] visited = new boolean[n];
         
         Queue<Integer> q = new LinkedList<>();
         
@@ -19,9 +19,10 @@ public class Solution {
             int tmp = q.poll();
             if(visited[tmp]) return false;
             visited[tmp] = true;
-            List<Integer> neis = map.get(tmp);
+            HashSet<Integer> neis = map.get(tmp);
             for(int ne: neis){
                 q.offer(ne);
+                map.get(ne).remove(tmp);
             }
         }
         
