07871641035c3a6c24ecdffd8d6b8b434fde89fc
seaskymonster <wenlaizhang@gmail.com>
2016-09-30 02:17:20 -0700

    [295. Find Median from Data Stream][Compile Error]committed by LeetCode Extension

---
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -0,0 +1,32 @@
+public class MedianFinder {
+    public PriorityQueue<Integer> minHeap;
+    public PriorityQueue<Integer> maxHeap;
+    
+    MedianFinder(){
+        mixHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    }
+
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+       maxHeap.offer(num);
+       minHeap.offer(maxHeap.poll());
+       if(maxHeap.size() < minHeap.size()){
+           maxHeap.offer(minHeap.poll());
+       }
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if(maxHeap.size() == minHeap.size()){
+            return (maxHeap.peek() + minHeap.peek())/2.0;
+        }else{
+            return maxHeap.peek();
+        }
+    }
+};
+
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf = new MedianFinder();
+// mf.addNum(1);
+// mf.findMedian();
