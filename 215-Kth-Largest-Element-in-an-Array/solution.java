103ba0fd68a9c119cde1027235fab856c203281e
seaskymonster <wenlaizhang@gmail.com>
2016-10-01 15:17:58 -0700

    [215. Kth Largest Element in an Array][Time Limit Exceeded]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -43,16 +43,52 @@
 
 
 public class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(k);
-        for(int i = 0; i < nums.length; i++){
-            pq.offer(nums[i]);
-            if(pq.size() > k) {
-                pq.poll();
-            }
-        }
+    // public int findKthLargest(int[] nums, int k) {
+    //     int n = nums.length;
+    //     PriorityQueue<Integer> pq = new PriorityQueue<Integer>(k);
+    //     for(int i = 0; i < nums.length; i++){
+    //         pq.offer(nums[i]);
+    //         if(pq.size() > k) {
+    //             pq.poll();
+    //         }
+    //     }
         
-        return pq.poll();
+    //     return pq.poll();
+    // }
+    
+    public int findKthLargest(int[] nums, int k) {
+       int start = 0;
+       int end = nums.length -1;
+       int position = nums.length - k +1; // the position th smallest.
+       int p = partition(nums, start, end, position);
+       return nums[p];
     }
+    
+    public int partition(int[] nums, int start, int end, int k){
+        if(start == end) return start;
+        int pivot = nums[(start + end)/2];
+        int i = start;
+        int j = end;
+        while(i < j){
+            while(i < j && nums[i] < pivot){
+                i++;
+            }
+            
+            while(i < j && nums[j] > pivot){
+                end --;
+            }
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+        if( i +1 == k){
+            return i;
+        }else if(i + 1 < k){
+            return partition(nums, i+1, end, k);
+        }else{
+            return partition(nums, start, i-1, k);
+        }
+    }
+    
+    
 }
