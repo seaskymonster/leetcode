2c6a2dd5971151389723686b00ac7ff4f380dd25
seaskymonster <wenlaizhang@gmail.com>
2016-10-02 15:00:14 -0700

    [138. Copy List with Random Pointer][Runtime Error]committed by LeetCode Extension

--- 138-Copy-List-with-Random-Pointer/solution.java
+++ 138-Copy-List-with-Random-Pointer/solution.java
@@ -8,31 +8,27 @@
  */
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
-        // 我们可以用hashmap存 old node - new node， 那样的话，第二遍扫描的时候，我们可以根据 原来list找到random node， 然后对应找到新的node
-        if(head == null){
-            return null;
-        }
-        HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
-        RandomListNode newhead = new RandomListNode(head.label);
-        map.put(head, newhead);
-        RandomListNode pre = newhead;
-        RandomListNode node = head.next;
-        // 以上是创建新的head，让pre 等于head，然后下边就开始loop，一遍loop 一遍把两个list对应的点存到map里面
-        while(node != null){
-            RandomListNode newnode = new RandomListNode(node.label);
-            map.put(node, newnode);
-            pre.next = newnode;
-            pre = newnode;
-            node = node.next;
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
+        RandomListNode cur = head;
+        RandomListNode newHead = new RandomListNode(head.label);
+        RandomListNode newCur = newHead;
+        while(cur.next != null){
+            newCur.next = new RandomListNode(cur.next.label);
+            map.put(cur.next, newCur.next);
+            cur = cur.next;
+            newCur = newCur.next;
         }
         
-        RandomListNode newnode = newhead;
-        node = head;
-        while(node != null){
-            newnode.random = map.get(node.random);
-            newnode = newnode.next;
-            node = node.next;
+        cur = head;
+        newCur = newHead;
+        
+        while(cur != null){
+            RandomListNode curRandom = cur.random;
+            RandomListNode newCurRandom = map.get(curRandom);
+            newCur.random = newCurRandom;
+            cur = cur.next;
+            newCur = newCur.next;
         }
-        return newhead;
+        return newHead;
     }
 }
