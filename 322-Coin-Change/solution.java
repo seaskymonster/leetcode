3a6fda5b060d4795ebbbe6270e60929e36f2098f
seaskymonster <wenlaizhang@gmail.com>
2016-10-02 22:22:59 -0700

    [322. Coin Change][Accepted]committed by LeetCode Extension

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -3,19 +3,19 @@ public class Solution {
         int[] f = new int[amount +1];
         Arrays.fill(f, -1);
         f[0] = 0;
-        for(int i = 1 ; i <= amount; i++){
-            for(int j =0; j < coins.length; j++){
-                // System.out.println('i'+i);
-                // System.out.println('j'+j);
-                if(i- coins[j] >= 0 && f[i -coins[j]] != -1){
-                    if(f[i] == -1){
+        
+        
+        for(int i = 1; i < f.length; i++){
+            for(int j = 0; j < coins.length; j++){
+                if(i- coins[j] >= 0 && f[i- coins[j]] != -1){
+                     if(f[i] == -1){
                         f[i] = f[i - coins[j]] + 1;
                     }else{
                         f[i] = Math.min(f[i - coins[j]] +1, f[i]);
                     }
-                }  
+                }
             }
         }
-        return f[amount];
+         return f[amount];
     }
 }
